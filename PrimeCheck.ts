f8f5f696230ddec47d53dc28d5fc942e7b9f3cf5
Pranoy Biswas Bappa <75092679+impranoybiswas@users.noreply.github.com>
2026-03-10 15:07:34 +0600

    add prime check

---
+++ PrimeCheck.ts
@@ -0,0 +1,11 @@
+function primeCheck(n: number) {
+  if (n < 1) return false; // prime start from 2
+  if (n === 2) return true; // if number is two
+  if (n % 2 === 0) return false; //for even
+  for (let i = 3; i <= Math.sqrt(n); i += 2) {
+    if (n % i === 0) return false;
+  }
+  return true;
+}
+
+console.log("isPrime : ", primeCheck(4));
